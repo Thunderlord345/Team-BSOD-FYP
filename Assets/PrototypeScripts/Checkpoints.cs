0c96af41bca2e57cba719f4c192f7d9bdb6cb9af
mxluvgdd <104023159+mxluvgdd@users.noreply.github.com>
2023-05-02 02:19:50 +0800

    2nd idea(Journey) completed

--- Assets/PrototypeScripts/Checkpoints.cs
+++ Assets/PrototypeScripts/Checkpoints.cs
@@ -14,21 +14,12 @@ public class Checkpoints : MonoBehaviour
 
     public GameObject panel;
 
+    bool isTriggered = false; //disable opening again
+
     // Start is called before the first frame update
     void Start()
     {
-        switch (checkpoints)
-        {
-            case Checkpoint.c1:
-                panel.SetActive(true);
-                break;
-            case Checkpoint.c2:
-                panel.SetActive(true);
-                break;
-            case Checkpoint.c3:
-                panel.SetActive(true);
-                break;
-        }
+        
     }
 
     // Update is called once per frame
@@ -39,9 +30,19 @@ public class Checkpoints : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !isTriggered) //only open if triggered is not true
         {
-
+            print("openingPanel");
+            isTriggered = true;
+            panel.SetActive(true);
+            Time.timeScale = 0;
         }
+    }
+
+    public void Back()
+    {
+        panel.SetActive(false); //for button to deactivate
+        Time.timeScale = 1;
+
     }
 }
--- Assets/PrototypeScripts/MainMenu.cs
+++ Assets/PrototypeScripts/MainMenu.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class MainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Back()
+    {
+
     }
 }
