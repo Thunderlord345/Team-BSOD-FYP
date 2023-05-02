be24859520b69c4584307dc0c486829aac9df1b9
MinXuan <liewminxuan@gmail.com>
2023-05-02 15:36:15 +0800

    scriptable obj added & shop money linked

--- Assets/PrototypeScripts/Shop.cs
+++ Assets/PrototypeScripts/Shop.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+//using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-
+    public TMP_Text moneyUI;
     int money; //money in game 
     //Text amount;
     
@@ -12,7 +14,8 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        money = 100; 
+        moneyUI.text = "$" + money.ToString();
         
     }
 
