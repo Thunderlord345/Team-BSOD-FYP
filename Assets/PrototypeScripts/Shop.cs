58e365f420360f8a85555db2fd5663176c3aa8e5
MinXuan <liewminxuan@gmail.com>
2023-05-02 15:50:37 +0800

    fking scuffed purchase system w/o inventory
    iw to sleep

--- Assets/PrototypeScripts/Shop.cs
+++ Assets/PrototypeScripts/Shop.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    public TMP_Text moneyUI;
+    public TMP_Text moneyUI;  //Text amount;
     int money; //money in game 
-    //Text amount;
-    
+
+    public Button purchaseBut; //purchase button 
 
     // Start is called before the first frame update
     void Start()
     {
-        money = 100; 
-        moneyUI.text = "$" + money.ToString();
+        money = 100;
+        UpdateBank();
         
     }
 
@@ -27,10 +27,18 @@ public class Shop : MonoBehaviour
 
     public void Purchase()
     {
-        //Once pressed money reduce
+        //After purchasing money goes down 
+        money -= 10; //reduce by 10
+        purchaseBut.interactable = false;
+        UpdateBank();
+
 
         //item get added into a slot 
 
+    }
 
+    void UpdateBank() //making sure new amount is updated 
+    {
+        moneyUI.text = "$" + money.ToString();
     }
 }
