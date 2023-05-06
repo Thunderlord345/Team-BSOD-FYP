2f9fac322e33cf666468aa462cab49a8dc60235b
mxluvgdd <104023159+mxluvgdd@users.noreply.github.com>
2023-05-07 04:13:12 +0800

    ok p much all prototype is ok
    -aesthetic needs to be better
    -level select just 3 buttons

---
+++ Assets/PrototypeScripts/LevelTrans.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTrans : MonoBehaviour
+{
+   public void LevelSelect()
+    {
+        //SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Main()
+    {
+        SceneManager.LoadScene("Walking&Progress");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene("LevelSelect");
+        }
+    }
+}
