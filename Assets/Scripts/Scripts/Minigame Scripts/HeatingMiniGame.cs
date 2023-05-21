15477adee6a1adcc9580c9b3773282c2a1093da3
Thunderlord345 <gregoryganis@gmail.com>
2023-05-21 17:36:06 +0800

    HeatingMinigame test

--- Assets/Scripts/Scripts/Minigame Scripts/HeatingMiniGame.cs
+++ Assets/Scripts/Scripts/Minigame Scripts/HeatingMiniGame.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class HeatingMiniGame : MonoBehaviour
 {
+    [SerializeField] Transform topPivot;
+    [SerializeField] Transform botPivot;
+
+    [SerializeField] Transform meter;
+    float meterSpeed;
+    //float heatingDuration = 3f;
+    float heatingPull = 0.001f;
+    float heatingGrav = 0.0005f;
+    float meterPos;
+
+    float meterSize;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,30 @@ public class HeatingMiniGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cook();
+    }
+
+    void Cook()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            meterSpeed += heatingPull * Time.deltaTime;
+            print("Pulling");
+        }
+        meterSpeed -= heatingGrav * Time.deltaTime;
+        meterPos += meterSpeed;
+
+        if(meterPos - meterSize/2 <= 0f && meterSpeed < 0f)
+        {
+            meterSpeed = 0f;
+        }
+
+        if (meterPos - meterSize / 2 >= 1f && meterSpeed > 0f)
+        {
+            meterSpeed = 0f;
+        }
+
+        meterPos = Mathf.Clamp(meterPos, 0, 1);
+        meter.position = Vector3.Lerp(botPivot.position, topPivot.position,meterPos );
     }
 }
