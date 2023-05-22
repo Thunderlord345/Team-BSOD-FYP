d9bdf3dc3436d8a7cbe65d3f549a4fcdc533992a
MinXuan <liewminxuan@gmail.com>
2023-05-22 14:17:49 +0800

    ingredients scriptable placed

--- Assets/Scripts/PrototypeScripts/ScriptableObj/Items.cs
+++ Assets/Scripts/PrototypeScripts/ScriptableObj/Items.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Items", menuName = "NewItems/Item")]
 public class Items : ScriptableObject
 {
+    public enum Item
+    {
+        //all items 
+    }
+
+    public Item item; //so things can refer to this
     public Sprite image;
     public string name;
     public string description;
