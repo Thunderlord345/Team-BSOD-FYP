d4a70641996052c175b9ac8c8a43e4c6e996321d
mxluvgdd <104023159+mxluvgdd@users.noreply.github.com>
2023-05-24 02:29:58 +0800

    basic interface for walking + dialogue box

--- Assets/Scripts/PrototypeScripts/Player/CameraFollow.cs
+++ Assets/Scripts/PrototypeScripts/Player/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    Vector3 offset = new Vector3(0f, 2f, -10f);
+    Vector3 offset = new Vector3(0f, 1f, -20f);
     float smoothTime = 0.25f;
     Vector3 velocity = Vector3.zero;
 
