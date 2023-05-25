1fdcc02f53b7622aebdf89f3c0c46aa729fcb613
Thunderlord345 <gregoryganis@gmail.com>
2023-05-25 15:53:03 +0800

    HeatingMinigame

--- Assets/Scripts/Minigame Scripts/HeatingMiniGame.cs
+++ Assets/Scripts/Minigame Scripts/HeatingMiniGame.cs
@@ -10,8 +10,8 @@ public class HeatingMiniGame : MonoBehaviour
     [SerializeField] Transform meter;
     float meterSpeed;
     //float heatingDuration = 3f;
-    [SerializeField] float heatingPull = 0.0005f;
-    [SerializeField] float heatingGrav = 0.0005f;
+    [SerializeField] float heatingPull = 0.05f;
+    [SerializeField] float heatingGrav = 0.005f;
     float meterPos;
 
     float meterPower;
@@ -39,12 +39,13 @@ public class HeatingMiniGame : MonoBehaviour
 
     void Meter()
     {
-        //if (Input.GetMouseButton(0))
-        //{
-        meterSpeed += heatingPull * Time.deltaTime;
-        print("Pulling");
+        if (Input.GetMouseButton(0))
+        {
+            meterSpeed += heatingPull * Time.deltaTime;
+            print("Pulling");
+            
+        }
         meterPos += meterSpeed;
-       // }
         meterSpeed -= heatingGrav * Time.deltaTime;
         
 
@@ -73,8 +74,8 @@ public class HeatingMiniGame : MonoBehaviour
 
         if (ht.white)
         {
-            meterPower = 0.01f;
-            heatingProgress += meterPower * Time.deltaTime;
+            meterDegradation = 0.06f;
+            heatingProgress -= meterDegradation * Time.deltaTime;
             
         } else if (ht.yellow)
         {
