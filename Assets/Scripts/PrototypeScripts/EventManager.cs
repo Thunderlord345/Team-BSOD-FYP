9066f43bb64912ea52f4b81080cbdd4748e2c66d
mxluvgdd <104023159+mxluvgdd@users.noreply.github.com>
2023-05-28 18:03:18 +0800

    added in explanation and moved spawn location

--- Assets/Scripts/PrototypeScripts/EventManager.cs
+++ Assets/Scripts/PrototypeScripts/EventManager.cs
@@ -12,22 +12,24 @@ public class EventManager : MonoBehaviour
     public Transform[] spawn; //location 
     //public Event weatherSpawn;
 
-    int spawnIndex = 0;
+    int spawnIndex = 0; 
 
     public void SpawnIngre()
     {
+        //check that there's still obj to spawn at a location 
+        if (ingre.Length == 0 || spawn.Length == 0)
+            return;
 
-
-        int randomObjectIndex = Random.Range(0, ingre.Length); //pick a random obj in the list 
-        
-        if(spawnIndex >= ingre.Length || spawnIndex >= spawn.Length)
+        //making sure it does not exceed the available spawn or obj
+        if (spawnIndex >= ingre.Length || spawnIndex >= spawn.Length)
         {
-            spawnIndex = 0; //making sure it does not exceed the available spawn or obj
+            spawnIndex = 0; 
         }
 
+        int randomObjectIndex = Random.Range(0, ingre.Length); //pick a random obj in the list 
+
         GameObject ingreSpawn = Instantiate(ingre[randomObjectIndex], spawn[spawnIndex].position, Quaternion.identity);
-        print(ingreSpawn);
-        ingreSpawn.transform.position = spawn[spawnIndex].position;
+        ingreSpawn.transform.position = spawn[spawnIndex].position; //set the spawned obj to the spawner location 
 
         RemoveSpawnPoint(spawnIndex);
         RemoveObject(randomObjectIndex);
@@ -36,14 +38,14 @@ public class EventManager : MonoBehaviour
     void RemoveSpawnPoint(int index)
     {
         var newSpawn = new List<Transform>(spawn);
-        newSpawn.RemoveAt(index);
-        spawn = newSpawn.ToArray();
+        newSpawn.RemoveAt(index); //remove transform that was used 
+        spawn = newSpawn.ToArray(); //set the new list 
     }
 
     void RemoveObject(int index)
     {
         var newIngre = new List<GameObject>(ingre);
-        newIngre.RemoveAt(index);
+        newIngre.RemoveAt(index); 
         ingre = newIngre.ToArray();
     }
 }
