f49ba8807bdae311d4fb086925565c4a5a15aaa5
MinXuan <liewminxuan@gmail.com>
2023-05-29 16:53:30 +0800

    added in proper plant prefab and set spawn

--- Assets/Scripts/PrototypeScripts/SpawnManager.cs
+++ Assets/Scripts/PrototypeScripts/SpawnManager.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using System.Linq;
 
-public class EventManager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
 
     public UnityEvent ingreSpawn;
